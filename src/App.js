48e6caae78f703357b5a36110b56e044437dd56f
Anteneh Solomon <antenehcs@gmail.com>
2022-04-27 09:58:59 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/about/About';
 import Resume from './components/resume/Resume';
 import Contact from './components/contact/Contact';
 import Projects from './components/Projects/Projects';
+import Break from './components/break/Break';
 
 
 
@@ -11,9 +12,13 @@ function App() {
   return (
     <div>
       <Home/>
+      <Break/>
       <About/>
+      <Break/>
       <Resume/>
+      <Break/>
       <Projects/>
+      <Break/>
       <Contact/>
     </div>
   );
---
+++ src/components/break/Break.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './break.css'
+
+const Break = () => {
+  return (
+    <div className='mainbreak'>
+        <div className='firstmain'>
+        </div>
+        <div className='secondmain'>
+        </div>
+        <div className='thirdmain'>
+        </div>
+    </div>
+  )
+}
+
+export default Break
+
